abdaaa142e185a07db9e61ef36c08d7a26295188
P1zz4br0etch3n <P1zz4br0etch3n@users.noreply.github.com>
2020-07-07 20:23:06 +0200

    second try to fix multi instance issue on mac

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -59,8 +59,8 @@ def open_playlist(path_to_playlist):
             path_to_propresenter = "C:\\Program Files (x86)\\Renewed Vision\\ProPresenter 6\\ProPresenter.exe"
             subprocess.Popen([path_to_propresenter, path_to_playlist], creationflags=DETACHED_PROCESS)
         elif is_mac():
-            path_to_propresenter = '"/Applications/ProPresenter 6.app"'
-            subprocess.Popen([path_to_propresenter, path_to_playlist], shell=True)
+            path_to_propresenter = '/Applications/ProPresenter 6.app'
+            subprocess.Popen(['open', '-a', path_to_propresenter, path_to_playlist])
     except Exception as e:
         logger.error('open_playlist - Cannot open playlist: %s' % str(e))
 
